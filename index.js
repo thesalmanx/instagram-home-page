9bdb98feb9423d03cb76b00720e4b5b34562bafe
Muhammad Salman <111143401+thesalmanx@users.noreply.github.com>
2024-11-26 22:42:08 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+// let image = document.getElementById('img')
+// let images = ['screenshot2-2x.png', 'screenshot4-2x.png']
+
+// setInterval(function(){
+//     let random = Math.floor(Math.random() * 2);
+//     image.src = images[random];
+// }, 500)
